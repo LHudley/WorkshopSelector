eead2459d31aa9771cc1e0f82caee8456613b989
latoya hudley <b2sherran@yahoo.com>
2021-11-01 20:45:20 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace WorkshopSelector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btBooking = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblRegistration = new System.Windows.Forms.Label();
+            this.lblLodging = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblNumOfDays = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Handling Stress",
+            "Time Management",
+            "Supervision Skills",
+            "Negotiation",
+            "How to Interview"});
+            this.listBox1.Location = new System.Drawing.Point(34, 128);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(193, 95);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "Austin",
+            "Chicago",
+            "Dallas",
+            "Orlando",
+            "Phoenix",
+            "Raleigh"});
+            this.listBox2.Location = new System.Drawing.Point(328, 128);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(193, 95);
+            this.listBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(170, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Acne Training Workshop";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Choose a Workshop";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(325, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Location";
+            // 
+            // btBooking
+            // 
+            this.btBooking.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btBooking.Location = new System.Drawing.Point(34, 286);
+            this.btBooking.Name = "btBooking";
+            this.btBooking.Size = new System.Drawing.Size(75, 40);
+            this.btBooking.TabIndex = 5;
+            this.btBooking.Text = "Book Now";
+            this.btBooking.UseVisualStyleBackColor = true;
+            this.btBooking.Click += new System.EventHandler(this.btBooking_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(236, 286);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 40);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(446, 286);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 40);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblRegistration
+            // 
+            this.lblRegistration.AutoSize = true;
+            this.lblRegistration.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegistration.Location = new System.Drawing.Point(51, 363);
+            this.lblRegistration.Name = "lblRegistration";
+            this.lblRegistration.Size = new System.Drawing.Size(93, 15);
+            this.lblRegistration.TabIndex = 8;
+            this.lblRegistration.Text = "Registration: ";
+            this.lblRegistration.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblRegistration.Visible = false;
+            // 
+            // lblLodging
+            // 
+            this.lblLodging.AutoSize = true;
+            this.lblLodging.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLodging.Location = new System.Drawing.Point(51, 440);
+            this.lblLodging.Name = "lblLodging";
+            this.lblLodging.Size = new System.Drawing.Size(63, 15);
+            this.lblLodging.TabIndex = 9;
+            this.lblLodging.Text = "Lodging:";
+            this.lblLodging.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblLodging.Visible = false;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(51, 480);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(43, 15);
+            this.lblTotal.TabIndex = 10;
+            this.lblTotal.Text = "Total:";
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTotal.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(202, 355);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(262, 23);
+            this.label4.TabIndex = 11;
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(202, 440);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(262, 23);
+            this.label5.TabIndex = 12;
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Location = new System.Drawing.Point(202, 480);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(262, 23);
+            this.label6.TabIndex = 13;
+            this.label6.Visible = false;
+            // 
+            // lblNumOfDays
+            // 
+            this.lblNumOfDays.AutoSize = true;
+            this.lblNumOfDays.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumOfDays.Location = new System.Drawing.Point(51, 407);
+            this.lblNumOfDays.Name = "lblNumOfDays";
+            this.lblNumOfDays.Size = new System.Drawing.Size(137, 15);
+            this.lblNumOfDays.TabIndex = 14;
+            this.lblNumOfDays.Text = "Workshop Duration: ";
+            this.lblNumOfDays.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblNumOfDays.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Location = new System.Drawing.Point(202, 399);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(262, 23);
+            this.label8.TabIndex = 15;
+            this.label8.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 547);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblNumOfDays);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblLodging);
+            this.Controls.Add(this.lblRegistration);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btBooking);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btBooking;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblRegistration;
+        private System.Windows.Forms.Label lblLodging;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblNumOfDays;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkshopSelector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string selectedWorkshop;
+        string selectedCity;
+        int numOfDays;
+        decimal registration;
+        decimal lodging;
+        decimal fee;
+        decimal total;
+
+        private void btBooking_Click(object sender, EventArgs e)
+        {
+           
+           
+
+            lblRegistration.Visible = true;
+            lblLodging.Visible = true;
+            lblTotal.Visible = true;
+            lblNumOfDays.Visible = true;
+
+
+
+            if (listBox1.SelectedIndex != -1)
+            {
+                selectedWorkshop = listBox1.SelectedItem.ToString();
+
+
+
+                switch (selectedWorkshop)
+                {
+                    case "Handling Stress":
+                        label8.Text = "3 Days";
+                        label4.Text = "$1000";
+                        numOfDays = 3;
+                        registration = 1000m;
+                        break;
+                    case "Time Management":
+                        label8.Text = "3 Days";
+                        label4.Text = "$800";
+                        numOfDays = 3;
+                        registration = 800m;
+                        break;
+                    case "Supervision Skills":
+                        label8.Text = "3 Days";
+                        label4.Text = "$1500";
+                        numOfDays = 3;
+                        registration = 1500m;
+                        break;
+                    case "Negotiation":
+                        label8.Text = "5 Days";
+                        label4.Text = "$1300";
+                        numOfDays = 5;
+                        registration = 1300m;
+                        break;
+                    case "How to Interview":
+                        label8.Text = "1 Days";
+                        label4.Text = "$500";
+                        numOfDays = 1;
+                        registration = 500m;
+                        break;
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("No selected items");
+            }
+
+
+            if (listBox2.SelectedIndex != -1)
+            {
+                selectedCity = listBox2.SelectedItem.ToString();
+
+
+                switch (selectedCity)
+                {
+                    case "Austin":
+                        lodging = 150m;
+                        break;
+                    case "Chicago":
+                        lodging = 225m; ;
+                        break;
+                    case "Dallas":
+                        lodging = 175m;
+                        break;
+                    case "Orlando":
+                        lodging = 300m;
+                        break;
+                    case "Phoenix":
+                        lodging = 175m;
+                        break;
+                    case "Raleigh":
+                        lodging = 150m;
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("No selected items");
+            }
+
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+            label8.Visible = true;
+            fee = lodging * numOfDays;
+            total = fee + registration;
+            label5.Text = fee.ToString("c");
+            label6.Text = total.ToString("c");
+                
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+           
+
+
+            label4.Text = "";
+            label5.Text = "";
+            label6.Text = "";
+            label8.Text = "";
+
+            listBox1.ClearSelected();
+            listBox2.ClearSelected();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
